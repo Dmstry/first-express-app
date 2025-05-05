8571d4788ed14ec252769610d117f95d02eab1c2
Dmstry <solarr@ukr.net>
2025-05-06 02:36:30 +0300

    Add contacts class

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const { format } = require('date-fns');
 
 const app = express();
 
@@ -7,12 +9,86 @@ app.get('/', (req, res) => {
 });
 
 // CRUD
-app.get('/posts', (req, res) => {});
-app.get('/posts/:id', (req, res) => {});
-app.post('/posts', (req, res) => {});
-app.patch('/posts/:id', (req, res) => {});
-app.delete('/posts/:id', (req, res) => {});
+app.get('/contacts', (req, res) => {});
+app.get('/contacts/:id', (req, res) => {});
+app.post('/contacts', (req, res) => {});
+app.patch('/contacts/:id', (req, res) => {});
+app.delete('/contacts/:id', (req, res) => {});
 
-app.get('/posts/:id/posts/256', (req, res) => {});
+//----------------------------------------------------------
+const contactsDB = [
+  {
+    id: 0,
+    name: 'Test',
+    telNumber: '+380123456789',
+    birthday: '2000-12-01',
+    isFavourite: false,
+  },
+  {
+    id: 1,
+    name: 'Test1',
+    telNumber: '+380123456788',
+    birthday: format(new Date(), 'y-MM-dd'),
+    isFavourite: true,
+  },
+];
+
+class ContactsDB {
+  constructor(arr) {
+    this.contacts = [...arr];
+  }
+
+  createContact(newContact) {
+    this.contacts.push({ ...newContact, id: uuidv4(), isFavourite: false });
+    return this.contacts[this.contacts.length - 1];
+  }
+
+  getContacts() {
+    return [...this.contacts];
+  }
+
+  getContactById(id) {
+    const foundIndex = this.contacts.findIndex((c) => c.id === Number(id));
+    return foundIndex === -1 ? null : this.contacts[foundIndex];
+  }
+
+  updateContact(id, values) {
+    const foundContactIndex = this.contacts.findIndex(
+      (c) => c.id === Number(id)
+    );
+    if (foundContactIndex !== -1) {
+      this.contacts[foundContactIndex] = {
+        ...this.contacts[foundContactIndex],
+        ...values,
+      };
+    }
+
+    return foundContactIndex === -1 ? null : this.contacts[foundContactIndex];
+  }
+
+  deleteContact(id) {
+    const foundContactIndex = this.contacts.findIndex(
+      (c) => c.id === Number(id)
+    );
+
+    return foundContactIndex === -1
+      ? null
+      : this.contacts.splice(foundContactIndex, 1);
+  }
+}
+
+const contactsDbInstance = new ContactsDB(contactsDB);
+
+// const createdContact = contactsDbInstance.createContact({
+//   name: 'Test',
+//   telNumber: '+380123456789',
+//   birthday: '2000-12-01',
+// });
+
+// const contacts = contactsDbInstance.getContacts();
+
+// console.log('createdContact :>> ', createdContact);
+
+// console.log('contacts :>> ', contacts);
 
 module.exports = app;
