250a84b38321956f744457dcd467ef87b07f8b94
Dmstry <solarr@ukr.net>
2025-05-06 14:12:37 +0300

    Add params, query

--- app.js
+++ app.js
@@ -67,11 +67,12 @@ const contactsDbInstance = new ContactsDB(contactsDB);
 
 const app = express();
 
+// Middleware to parse json to js-object
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello Express</h1>');
 });
-
-app.use(express.json());
 
 // CRUD
 app.get('/contacts', (req, res) => {
@@ -84,7 +85,36 @@ app.post('/contacts', (req, res) => {
   res.status(201).send(createdContact);
 });
 
-app.get('/contacts/:id', (req, res) => {});
+// GET localhost:5000/contacts/10?resutls=10&page=5
+// :id - параметр маршрута
+// ?results=10&page=5 - параметри рядка запиту
+
+app.get('/contacts/:id', (req, res) => {
+  const {
+    params: { id },
+    query: { results, page },
+  } = req;
+  console.log('req.params :>> ', req.params);
+  console.log('req.query :>> ', req.query);
+  res.status(200).send('OK');
+  // const contactsById = contactsDbInstance.getContactById(id);
+  // res.status(200).send(contactsById)
+});
+
+// звернутися до всіх замовлень певного користувача, зокрема до виконаних (від загального до конкретного)
+// users/5/orders?isDone=true
+// прописати обробник, в якому витягти id користувача і дані для фільтру замовлень
+
+app.get('/users/:id/orders', (req, res) => {
+  const {
+    params: { id },
+    query: { isDone },
+  } = req;
+
+  console.log('id, isDone :>> ', id, isDone);
+  res.status(200).send('OK!');
+});
+
 app.patch('/contacts/:id', (req, res) => {});
 app.delete('/contacts/:id', (req, res) => {});
 
