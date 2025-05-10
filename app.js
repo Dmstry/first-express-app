8504846f1f31f71571ee2f38cc14c48eae51cd45
Dmstry <solarr@ukr.net>
2025-05-11 01:27:31 +0300

    Add validation on createContact in middleware

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 const express = require('express');
 
 const { contactsController } = require('./controllers');
+const { validate } = require('./middleware');
 
 const app = express();
 
 // Middleware to parse json to js-object
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello Express</h1>');
-});
+app.get(
+  '/',
+  (req, res, next) => {
+    console.log('handler 1 :>> ');
+    next();
+  },
+  (req, res) => {
+    console.log('handler 2 :>> ');
+    res.send('<h1>Hello Express</h1>');
+  }
+);
 
 // CRUD
 app.get('/contacts', contactsController.getContacts);
 
-app.post('/contacts', contactsController.createContact);
+app.post(
+  '/contacts',
+  validate.validateContactOnCreate,
+  contactsController.createContact
+);
 
 app.get('/contacts/:id', contactsController.getContactById);
 
-app.patch('/contacts/:id', contactsController.updateContactById);
+app.patch(
+  '/contacts/:id',
+  validate.validateContactOnUpdate,
+  contactsController.updateContactById
+);
 
 app.delete('/contacts/:id', contactsController.deleteContact);
 
